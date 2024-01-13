40cf15d76d669380df12b098806945804e2d15ab
Rost <6880416@stud.nau.edu.ua>
2024-01-14 00:59:28 +0300

    fix

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,7 +2,7 @@
 defineProps({
   title: String,
   compound: String,
-  imageUrl: String,
+  Image: String,
   price: Number,
   isAdded: Boolean,
   onClickAdd: Function
@@ -13,7 +13,7 @@ defineProps({
   <div
     class="relative bg-white border border-slate-100 rounded-3xl p-8 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl"
   >
-    <img id="name" :src="imageUrl" alt="food" />
+    <img id="name" :src="Image" alt="food" />
     <h4>{{ title }}</h4>
     <p class="mt-2">{{ compound }}</p>
     <div class="flex justify-between mt-5">
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -12,7 +12,7 @@ const onClickAdd = () => {
     <Card
       compound="Склад: Яблука."
       title="Яблучні чіпси"
-      imageUrl="/public/Chips/AppleChips.jpg"
+      Image="/public/Chips/AppleChips.jpg"
       :price="120"
       :isAdded="true"
       :onClickAdd="onClickAdd"
