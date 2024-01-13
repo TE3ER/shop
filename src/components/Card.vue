6cc3cce0e55743c80d49b75f5decff48b4495b4c
Rost <6880416@stud.nau.edu.ua>
2024-01-13 23:56:04 +0300

    fix

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -13,7 +13,7 @@ defineProps({
   <div
     class="relative bg-white border border-slate-100 rounded-3xl p-8 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl"
   >
-    <img :src="imageUrl" alt="sneakers-1" />
+    <img :src="imageUrl" alt="food" />
     <h4>{{ title }}</h4>
     <p class="mt-2">{{ compound }}</p>
     <div class="flex justify-between mt-5">
