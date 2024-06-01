14217a77b219edf40ca76fca3ced3ad09156a7ba
Rost <6880416@stud.nau.edu.ua>
2024-06-01 16:39:02 +0300

    Fix

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,19 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+  plugins: [vue()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vue: ['vue'],
+          'vue-router': ['vue-router']
+        }
+      }
     }
   }
 })
